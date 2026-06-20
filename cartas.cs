99198656b22666f74693cee9b973142e6c94098a
JESUS EMMANUEL UCAN CHAN <a19201483@alumnos.uady.mx>
2026-06-19 23:45:21 -0600

    Clase

---
+++ cartas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruba_10
+{
+    internal class cartas
+    {
+    }
+}
